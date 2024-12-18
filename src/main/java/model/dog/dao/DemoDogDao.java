a34b138f84f29357143b0e0817d86be02e93a8ef
emanuele notebook <russoemanuele028@gmail.com>
2024-12-18 13:07:00 +0100

    added dao classes dummy

---
+++ src/main/java/model/dog/dao/DemoDogDao.java
@@ -0,0 +1,14 @@
+package model.dog.dao;
+
+import model.dog.Dog;
+
+import java.util.List;
+
+public class DemoDogDao extends DogDao{
+    @Override
+    public Dog load(int dogId){return null;};
+    public void add(Dog dog){};
+    public void delete(int dogId){};
+    public void searchDogsByBreed(String breed){};
+    public List<Dog> getAllDogs(){return null;};
+}
---
+++ src/main/java/model/dogadoptionrequest/dao/DbmsDogAdoptionRequestDao.java
@@ -0,0 +1,13 @@
+package model.dogadoptionrequest.dao;
+
+import model.dogadoptionrequest.DogAdoptionRequest;
+
+import java.util.List;
+
+public class DbmsDogAdoptionRequestDao extends DogAdoptionRequestDao{
+    @Override
+    public DogAdoptionRequest load(int darId){ return null;};
+    public void add(DogAdoptionRequest dogAdoptionRequest){};
+    public void delete(int darId){};
+    public List<DogAdoptionRequest> getAllKennelDogAdoptionRequest(int kennelId){return null;};
+}
