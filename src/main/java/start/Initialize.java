de46f2592bde52f4ac0b57684d0b37269deb02cf
emanuele notebook <russoemanuele028@gmail.com>
2024-12-19 13:05:55 +0100

    added cli implementation only for take test

---
+++ src/main/java/start/Initialize.java
@@ -0,0 +1,34 @@
+package start;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Initialize
+{
+    public Initialize(){}
+
+    public void init(){
+        int choice;
+        System.out.println("SCEGLI LA MODALITA' GRAFICA:");
+        System.out.println("1. GUI");
+        System.out.println("2. CLI");
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        try {
+            choice = Integer.parseInt(reader.readLine());
+            switch(choice){
+                case 1:
+                    System.setProperty("user_choice", "GUI");
+                    break;
+                    case 2:
+                        System.setProperty("user_choice", "CLI");
+                        break;
+                        default:
+                            System.setProperty("user_choice", "GUI");
+                            break;
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
