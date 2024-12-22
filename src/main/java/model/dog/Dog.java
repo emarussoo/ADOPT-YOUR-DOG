d6e15e43c6a522c4a4e52d36b922d2c6c737dbcb
emanuele notebook <russoemanuele028@gmail.com>
2024-12-22 18:50:44 +0100

    removed baseclasses DogBase

--- src/main/java/model/dog/Dog.java
+++ src/main/java/model/dog/Dog.java
@@ -1,10 +1,58 @@
 package model.dog;
 
-import baseclasses.DogBase;
+public class Dog{
 
-public class Dog extends DogBase {
+    private int dogId;
+    private String dogName;
+    private int dogAge;
+    private String dogBreed;
+    private int kennelId;
 
     public Dog(int dogId, String dogName, int dogAge, String dogBreed, int kennelId) {
-        super(dogId, dogName, dogAge, dogBreed, kennelId);
+        this.dogId = dogId;
+        this.dogName = dogName;
+        this.dogAge = dogAge;
+        this.dogBreed = dogBreed;
+        this.kennelId = kennelId;
+    }
+
+    public int getDogId() {
+        return dogId;
+    }
+
+    public void setDogId(int dogId) {
+        this.dogId = dogId;
+    }
+
+    public String getDogName() {
+        return dogName;
+    }
+
+    public void setDogName(String dogName) {
+        this.dogName = dogName;
+    }
+
+    public int getDogAge() {
+        return dogAge;
+    }
+
+    public void setDogAge(int dogAge) {
+        this.dogAge = dogAge;
+    }
+
+    public String getDogBreed() {
+        return dogBreed;
+    }
+
+    public void setDogBreed(String dogBreed) {
+        this.dogBreed = dogBreed;
+    }
+
+    public int getKennelId() {
+        return kennelId;
+    }
+
+    public void setKennelId(int kennelId) {
+        this.kennelId = kennelId;
     }
 }
